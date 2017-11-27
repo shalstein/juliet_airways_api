0224ecebdb0d2d5bf7b50776d5177d026b9cf2f0
Sholom Steinmetz <boruchsteinmetz@outlook.com>
2017-11-26 22:14:40 -0500

    modify find action and serializers for new db structure

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,19 +2,16 @@ class FlightsController < ApplicationController
 
   def find
     requested_departure_day = DateTime.parse(flight_params[:departure_date])
-    requested_departure_city = flight_params[:departure_city]
-    requested_arival_city = flight_params[:arival_city]
+    departure_airport_id = Airport.find_by(iata_code: flight_params[:departure_city]).id
+    arival_airport_id = Airport.find_by(iata_code: flight_params[:arival_city]).id
     sort_by = flight_params[:sort_by].nil? ? 'departure_datetime' : flight_params[:sort_by]
 
-
-    #unless requested_departure_day < DateTime.current || requested_departure_day > DateTime.current + 90.days
-      #flights = Flight.where departure_city: requested_departure_city, arival_city: requested_arival_city
-      flights = Flight.order(sort_by => :asc).where departure_city: requested_departure_city, arival_city: requested_arival_city, departure_datetime: between_beginning_to_end_of(requested_departure_day)
+      flights = Flight.joins(:route).where(routes: {departure_airport_id: departure_airport_id, arival_airport_id: arival_airport_id}, flights: {departure_datetime: between_beginning_to_end_of(requested_departure_day) } )
 
       if flights.empty?
         render json: {error: "Their are no flights on the queried date"}
       else
-        render json: flights, meta: {departure_city: requested_departure_city, arival_city: requested_arival_city, departure_date: Flight.format_date(requested_departure_day) }, meta_key: 'request'
+        render json: flights
       end
 
   end
@@ -45,5 +42,7 @@ class FlightsController < ApplicationController
   def between_beginning_to_end_of(date)
     date.beginning_of_day..date.end_of_day
   end
+
+
 
 end
--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,5 +1,6 @@
 class Airport < ApplicationRecord
 
+  
   def routes
     Route.where("arival_airport_id = ? OR departure_airport_id = ?", self.id, self.id)
   end
